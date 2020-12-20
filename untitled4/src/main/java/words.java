0fe32cfe46ca1c1fb86f73b8f735e1f06cc5e215
Warol Kojtyła <58826010+PapiezPolak2137@users.noreply.github.com>
2020-12-20 20:49:14 +0100

    Delete words.java

--- untitled4/src/main/java/words.java
+++
@@ -1,9 +0,0 @@
-public class words {
-    public static int countWordsUsingSplit(String input) {
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-        String[] words = input.split("\\s+"); return words.length;
-    }
-
-}
