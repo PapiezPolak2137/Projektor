1b4709566e22d6ba239aca3de3d9b9c9001ffa74
Warol Kojtyła <58826010+PapiezPolak2137@users.noreply.github.com>
2020-12-20 20:48:39 +0100

    Add files via upload

--- untitled4/src/main/java/Main.java
+++ untitled4/src/main/java/Main.java
@@ -1,36 +1,17 @@
 import org.jsoup.Jsoup;
-import java.util.ArrayList;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        Document doc = null;
-        try{
-            doc = Jsoup.connect("https://www.warframe.com/game/warframes").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Element element1 = doc.getAllElements().first();
 
-        Elements elements1 = element1.getAllElements();
+        System.out.println(Down.main().get(8));
 
-        ArrayList<String> rama = new ArrayList<String>();
+        CreateFile.main();
 
-        for(int i = 0; i >= 0; i++) {
-
-            try{
-                rama.add(element1.getElementsByClass("InnerWfTitle").get(i).text().toUpperCase());
-            } catch (IndexOutOfBoundsException e) {
-                break;
-            }
-
-        }
-
-        System.out.println(rama);
     }
-
 }
