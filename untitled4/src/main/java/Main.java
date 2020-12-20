a7405f7b56098ec29a600d2b3620c9bda3fac999
Warol Kojtyła <58826010+PapiezPolak2137@users.noreply.github.com>
2020-12-20 19:31:21 +0100

    Add files via upload

--- untitled4/src/main/java/Main.java
+++ untitled4/src/main/java/Main.java
@@ -15,25 +15,15 @@ public class Main {
             e.printStackTrace();
         }
         Element element1 = doc.getAllElements().first();
-        //System.out.println(element1.toString());
 
         Elements elements1 = element1.getAllElements();
 
         ArrayList<String> rama = new ArrayList<String>();
 
-        //System.out.println(elements1.get(1));
-        //System.out.println(element1.getElementById("loginLightbox"));
-        //System.out.println(element1.getElementById("jakiesid").text());
-        //System.out.println(element1.getElementsByIndexEquals(4));
-        //System.out.println(element1.select("td").get(10).text());
-        //System.out.println(element1.select("td").attr("class","base_txt").first());
-
-        boolean nwm = false;
-
         for(int i = 0; i >= 0; i++) {
 
             try{
-                rama.add(element1.getElementsByClass("InnerWfTitle").get(i).text());
+                rama.add(element1.getElementsByClass("InnerWfTitle").get(i).text().toUpperCase());
             } catch (IndexOutOfBoundsException e) {
                 break;
             }
